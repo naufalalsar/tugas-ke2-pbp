33424b74ff51f702d60d2675570e5f317a872f91
naufalalsar <muhammad.naufal120@ui.ac.id>
2022-09-16 00:59:47 +0700

    adding test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+def test_url_html(self):
+    response = self.get.client('/mywatchlist/html/')
+    self.assertEqual(response.status_code, 200)
+
+def test_url_xml(self):
+    response = self.get.client('/mywatchlist/xml/')
+    self.assertEqual(response.status_code, 200)
+
+def test_url_json(self):
+    response = self.get.client('/mywatchlist/json/')
+    self.assertEqual(response.status_code, 200)
